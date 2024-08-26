dc75c081621e95a07bac728de0c197ab2a50c003
Aditya-Gupta-ctrl <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-08-26 23:32:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ with st.sidebar:
             sac.MenuItem('@1', icon='', href=''),
             sac.MenuItem('@2', icon='', href=''),
         ]),
-    ], size='lg', variant='left-bar', color='grape', open_all=False, return_index=True)
+    ], size='lg', variant='left-bar', color='grape', open_all=True, return_index=True)
 
 
 #Home bar

b2764569c7c8d5ab370112c8023edfef03325d22
Aditya-Gupta-ctrl <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-08-28 15:23:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -289,10 +289,10 @@ if selected == 3:
             # Create a pandas DataFrame for each column
             import pandas as pd
             for i, col in enumerate(cols):
-                #col_df = pd.DataFrame([y_pred[i:i + num_cols] for i in range(0, len(y_pred), num_cols)])
-                #col_df.columns = [f"Column {i+1}" for i in range(num_cols)]
+                #col_df = pd.DataFrame([y_pred[i:i + num_cols] for i in range(0, len(y_pred), num_cols)]).T
+                #col_df.columns = [f"Column {i+1}" for i in range(col_df.shape[1])]
                 col_df = pd.DataFrame([y_pred[i:i + num_cols] for i in range(0, len(y_pred), num_cols)]).T
-                col_df.columns = [f"Column {i+1}" for i in range(col_df.shape[1])]
+                single_col_df = col_df.iloc[:, 0]
                 col.write(col_df)
     
             #st.subheader("Prediction Result")

b037c05d465bd66474d5103c00cea18e266df3e2
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-16 09:32:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -176,7 +176,7 @@ if selected == 3:
             x_column = st.selectbox('Select X-axis column', columns)
             y_column = st.selectbox('Select Y-axis column', columns)
             color_column = st.selectbox('Select Color column', columns)
-            st.scatter_chart(data, x=x_column, y=y_column, color=color_column)
+            st.scatter_chart(data, x=x_column, y=y_column, color='sepecies')
         else:
             st.write("Please select a dataset with at least two columns to display a scatter chart.")
 
